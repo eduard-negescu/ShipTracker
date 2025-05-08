0726999acfb2c3d20fd6729a795adbe883f3e498
eduard-negescu <eduardnegescu@gmail.com>
2025-05-08 22:37:38 +0300

    Removed Angular Project

--- shiptracker.client/src/app/app.component.ts
+++ shiptracker.client/src/app/app.component.ts
@@ -1,27 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { loadCountries } from './store/action';
-import { selectCountries, selectLoading } from './store/selector';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: false
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = 'shiptracker.client';
-
-  countries$!: Observable<any>;
-  loading$!: Observable<boolean>;
-
-  constructor(private http: HttpClient, private store: Store) { }
-
-  ngOnInit() {
-    this.store.dispatch(loadCountries());
-    this.countries$ = this.store.select(selectCountries);
-    this.loading$ = this.store.select(selectLoading);
-  }
+export class AppComponent {
+  title = 'my-app';
 }
