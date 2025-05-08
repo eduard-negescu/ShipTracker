616c8993030b03ac32c09e89d9ee149ec95d5151
eduard-negescu <eduardnegescu@gmail.com>
2025-05-08 22:42:43 +0300

    Actually removed Angular Frontend from app

--- shiptracker.client/src/app/store/action.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from "@ngrx/store";  
-import { Country } from "./model";  
-
-export const loadCountries = createAction("[Countries] Load Countries");  
-
-export const loadCountriesSuccess = createAction(  
- "[Countries] Load Countries Success",  
- props<{ countries: Country[] }>()  
-);
